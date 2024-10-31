aec2a99e429e4601e574507cc1dd050f31decb4c
david21blox <davidyguillemora@gmail.com>
2024-10-31 18:11:00 -0300

    Añade optimizaciones completas y configuraciones avanzadas

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
     try:
         tf.config.experimental.set_memory_growth(gpus[0], True)
-        tf.config.experimental.set_virtual_device_configuration(gpus[0], 
-            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4096)])
+        tf.config.experimental.set_virtual_device_configuration(
+            gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4096)]
+        )
         logical_gpus = tf.config.experimental.list_logical_devices('GPU')
         print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
     except RuntimeError as e:
